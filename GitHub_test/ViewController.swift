0d3628e2595a6f4084122ec7f809404d25cec917
Jirka Šmela <jiri.smela1@gmail.com>
2020-04-28 22:31:54 +0200

    Connect with ViewController

--- GitHub_test/ViewController.swift
+++ GitHub_test/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorCubeView: UIView!
+    @IBOutlet weak var RGBValueLabel: UILabel!
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBAction func slidersRGB(_ sender: UISlider) {
+      
+    }
+    @IBOutlet weak var textFont: UILabel!
+    @IBOutlet weak var fontPickerView: UIPickerView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
